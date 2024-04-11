c0d945abae13810145b3749132795c1a5d28bb72
jobel v. golde <jobelgolde43@gmail.com>
2024-04-12 06:18:57 +0800

    First commit

---
+++ script.js
@@ -0,0 +1,36 @@
+// const themeSun = document.querySelector('.bi-sun');
+//     const themeMoon = document.querySelector('.bi-moon');
+//     const body = document.body;
+//     const header = document.querySelector(".header");
+//     const sections = document.querySelectorAll("section");
+//     const navLinks = document.querySelectorAll(".nav-links a");
+
+// themeMoon.addEventListener('click',() =>{
+//   themeSun.style.display = 'block'
+//   themeMoon.style.display = 'none'
+
+//  document.querySelector('.text-decoration-none').classList.add('text-light')
+//  document.querySelector('.text-decoration-none').classList.remove('text-dark')
+
+
+// })
+// themeSun.addEventListener('click',() =>{
+//     themeSun.style.display = 'none'
+//     themeMoon.style.display = 'block'
+
+//     body.classList.remove("bg-dark");
+//         body.classList.add("bg-light", "text-dark");
+//         header.classList.add("header-bg");
+//         sections.forEach(section => {
+//             section.classList.remove("bg-dark", "text-dark");
+//             section.classList.add("bg-light", "text-light");
+//         });
+//         navLinks.forEach(navLink => {
+//             navLink.classList.remove("text-dark");
+//             navLink.classList.add("text-light");
+//         });
+//     });
+
+
+
+  
